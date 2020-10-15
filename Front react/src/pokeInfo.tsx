469f20b14bed0aeb78e449266044e0f8a807f725
MestrieEsteban <esteban94.em@gmail.com>
2020-10-15 12:29:10 +0200

    ok

--- Front react/src/pokeInfo.tsx
+++ Front react/src/pokeInfo.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./pokecard.css";
+var request = require("request");
+
 
 export default class Pokemons extends React.Component<{}, any> {
   constructor(props: string) {
     super(props);
     this.state = {
       pokemons: [],
-      pokeId: this.props,
+	  pokeId: this.props,
+	  name: '',
+	  description: '',
     };
   }
 
@@ -15,7 +19,6 @@ export default class Pokemons extends React.Component<{}, any> {
   }
 
   async fetchListPokemons() {
-    var request = require("request");
     var options = {
       method: "GET",
       url: `http://localhost:5000/api/pokemons${this.state.pokeId.location.pathname}`,
@@ -29,31 +32,75 @@ export default class Pokemons extends React.Component<{}, any> {
     });
   }
 
-  changeName(event : any) {
-	console.log(event.target.value);	
+  changeName(event: any) {
+    var options = {
+      method: "PUT",
+      url: "http://localhost:5000/api/pokemons/1",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        name: event.target.value,
+      },
+    };
+    request(options, (error: string | undefined, response: { body: any }) => {
+	  if (error) throw new Error(error);
+    });
+  }
+
+  changeDesc(event: any) {
+    var request = require("request");
+    var options = {
+      method: "PUT",
+      url: "http://localhost:5000/api/pokemons/1",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        description: event.target.value,
+      },
+    };
+    request(options, (error: string | undefined, response: { body: any }) => {
+	  if (error) throw new Error(error);
+    });
   }
 
   render() {
-	const pokemon = this.state.pokemons;
-	console.log(pokemon);
-	if(pokemon)
-    return (
-      <div className="poke-container">
-        <div className={"pokemon "}>
+    const pokemon = this.state.pokemons;
+    if (pokemon)
+      return (
+        <div className="poke-container">
+          <div className={"pokemon "}>
             <div className="img-container">
               <img src={pokemon.sprite} alt={pokemon.name} />
             </div>
             <div className="info">
-              <span className="number">
-				  #{pokemon.pokeId}
-              </span>
-			  <br/>
-              <input type="text" value={pokemon.name} name="name" onChange={this.changeName}/><br/>
-              <small className="type">{pokemon.types}</small> <br/>
-              <input type="text" value={pokemon.description} name="description"/><br/>
+              <span className="number">#{pokemon.pokeId}</span>
+              <br />
+              <input
+                type="text"
+                value={this.state.name === "" ? pokemon.name : this.state.name}
+                name="name"
+				onChange={e =>{
+					this.changeName(e)
+					this.setState({name: e.target.value})
+				}}
+              />
+              <br />
+              <small className="type">{pokemon.types}</small> <br />
+              <input
+                type="text"
+                value={this.state.description === "" ? pokemon.description : this.state.description}
+				name="description"
+				onChange={e =>{
+					this.changeDesc(e)
+					this.setState({description: e.target.value})
+				}}
+              />
+              <br />
             </div>
+          </div>
         </div>
-      </div>
-    );
+      );
   }
 }
